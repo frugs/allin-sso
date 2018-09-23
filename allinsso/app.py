dda740e577bc2317c75bdc1a243ca4261c66af13
Hugo Wainwright <hugowainwright@fb.com>
2018-09-23 13:13:08 +0100

    refresh token to lib

--- allinsso/app.py
+++ allinsso/app.py
@@ -1,10 +1,10 @@
 """This is the single sign-on app"""
-from typing import Tuple
 
 import flask
 import flask_oauthlib.client
-
 from google.cloud import datastore
+
+from .discordoauth import refresh_discord_token, create_discord_remote_app
 
 
 def retrieve_config_value(key: str) -> str:
@@ -23,54 +23,17 @@ app.config["SESSION_COOKIE_HTTPONLY"] = True
 
 oauth = flask_oauthlib.client.OAuth(app)
 
-discord = oauth.remote_app(
-    "discord",
-    consumer_key=DISCORD_CLIENT_KEY,
-    consumer_secret=DISCORD_CLIENT_SECRET,
-    request_token_params={"scope": "identify connections"},
-    base_url="https://discordapp.com/api/v6/",
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url="https://discordapp.com/api/oauth2/token",
-    authorize_url="https://discordapp.com/api/oauth2/authorize",
-    access_token_headers={
-        "User-Agent": "Mozilla/5.0"
-    })
-
+discord = create_discord_remote_app(oauth, DISCORD_CLIENT_KEY, DISCORD_CLIENT_SECRET)
 
 def discord_auth_headers(access_token: str) -> dict:
     return {"Authorization": "Bearer " + access_token}
-
-
-def do_discord_refresh_token(refresh_token: str) -> Tuple[str, str]:
-    resp = discord.post(
-        discord.access_token_url,
-        token="token",
-        headers={"User-Agent": "Mozilla/5.0"},
-        data={
-            "grant_type": "refresh_token",
-            "client_id": discord.consumer_key,
-            "client_secret": discord.consumer_secret,
-            "refresh_token": refresh_token,
-        })
-
-    if resp.status == 200:
-        return resp.data.get("access_token", ""), resp.data.get("refresh_token", "")
-    else:
-        return "", ""
 
 
 @app.route("/")
 def index():
     """This is the main landing page for the app"""
 
-    access_token, refresh_token = do_discord_refresh_token(
-        flask.session.get("discord_refresh_token", ""))
-
-    if refresh_token:
-        flask.session["discord_refresh_token"] = refresh_token
-
-    if access_token:
+    if refresh_discord_token(discord, flask.session):
         return flask.redirect(POST_LOGIN_REDIRECT_PATH)
     else:
         return flask.render_template("index.html.j2")
@@ -105,19 +68,3 @@ def discord_authorised():
     flask.session["discord_refresh_token"] = resp["refresh_token"]
 
     return flask.redirect(POST_LOGIN_REDIRECT_PATH)
-
-
-@app.route("/discord-refresh-token", methods=["POST"])
-def discord_refresh_token():
-    """Endpoint for refreshing discord access token"""
-
-    if not flask.request.is_json:
-        return "Bad request", 400
-
-    access_token, refresh_token = do_discord_refresh_token(
-        flask.request.json.get("discord_refresh_token", ""))
-
-    return flask.jsonify({
-        "discord_access_token": access_token,
-        "discord_refresh_token": refresh_token
-    })
