66943179c26c65a6a91b94c6240e57764b1dc72f
Hugo Wainwright <hugowainwright@fb.com>
2019-10-04 23:32:54 +0100

    switch to https

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from allinsso import app
 
 
 def main():
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True, ssl_context='adhoc')
 
 
 if __name__ == "__main__":
